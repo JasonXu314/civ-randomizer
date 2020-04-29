544db2dd884eb221db4ab500084e0fdead5c7aab
Jason Xu <Xinwen31415@gmail.com>
2020-04-29 02:50:27 -0500

    Some progress with fetching data from backend

--- scripts.js
+++ scripts.js
@@ -1,21 +1,24 @@
 const civs = [];
 
-$.getJSON('https://baconipsum.com/api/?type=meat-and-filler&paras=5').then((res) => {
-	res.forEach((description) => {
+$.getJSON('https://civ-randomizer-backend.herokuapp.com/').then((res) => {
+	res.forEach((civ) => {
 		$(`
 			<li class = "civ">
-				<h3>${description.split(' ')[0]}</h3>
-				<img src = "https://vignette.wikia.nocookie.net/civilization/images/7/70/Teddy_Roosevelt_%28Civ6%29.png/" alt = "img here">
-				<p>${description.split(' ').slice(1).join(' ')}</p>
+				<img src = "${civ.icon}" alt = "${civ.name} icon">
+				<h3>${civ.name}</h3>
 			</li>
 		`).click((evt) => {
 			console.log(evt.currentTarget.querySelector('h3').textContent);
 		}).appendTo($('ul#civ-list'));
-		civs.push(description.split(' ')[0]);
+		civs.push(civ.name);
 	});
 });
 
 $('button#randomize-button').click(() => {
 	const choice = Math.floor(Math.random() * civs.length);
-	$('div#chosen-civ h2').text(civs[choice]);
+	$.getJSON('https://civ-randomizer-backend.herokuapp.com/').then((res) => {
+		// V WIP V \\
+		$('div#chosen-civ h2').text(civs[choice]);
+		$('div#chosen-civ img').append();
+	});
 });
