377bcfc37ea96e1b6c2c193ad4f9d00ee1f532a2
Jason Xu <Xinwen31415@gmail.com>
2020-04-30 01:05:30 -0500

    Added list of ablities, handling cases of multiple leaders

--- scripts.js
+++ scripts.js
@@ -16,9 +16,25 @@ $.getJSON('https://civ-randomizer-backend.herokuapp.com/').then((res) => {
 
 $('button#randomize-button').click(() => {
 	const choice = Math.floor(Math.random() * civs.length);
-	$.getJSON('https://civ-randomizer-backend.herokuapp.com/').then((res) => {
-		// V WIP V \\
-		$('div#chosen-civ h2').text(civs[choice]);
-		$('div#chosen-civ img').append();
+	$.getJSON(`https://civ-randomizer-backend.herokuapp.com/?civ=${civs[choice]}`).then((civ) => {
+		civ.leader = civ.leader[Math.floor(Math.random() * civ.leader.length)];
+		const civCard = $(`
+			<div class = "outer">
+				<div>
+					<h2>${civ.name}</h2>
+					<img src = "${civ.icon}" alt = "${civ.name} icon">
+				</div>
+				<div>
+					<h2>${civ.leader.name}</h2>
+					<img src = "${civ.leader.portrait}" alt = "${civ.leader.name}">
+				</div>
+			</div>
+			<h3>${civ.abilityName}</h3>
+			<p>${civ.ability}</p>
+			${civ.infrastructure.map((inf) => `<h3>${inf.name}</h3>\n<ul>${inf.effects.map((effect) => `<li>${effect}</li>`).join('\n')}</ul>`).join('\n')}
+			${civ.unit.map((unit) => `<h3>${unit.name}</h3>\n<ul>${unit.special.map((special) => `<li>${special}</li>`).join('\n')}</ul>`).join('\n')}
+		`);
+		$('#chosen-civ').children().remove();
+		$('#chosen-civ').append(civCard);
 	});
 });
